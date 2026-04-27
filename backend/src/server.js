a51289f139acb0a92bf79ba70c69ea389dedd4b9
dinukab <dinukasandaruwan487@gmail.com>
2026-04-27 12:17:00 +0530

    remove old seed.js file and add new seed.ts file

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,6 +25,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.use("/api/cart", cartRoutes);
 app.use("/api/auth", authRoutes);
